a0824d60f9e160c82794915ead10d8c37f601542
Alec Woodruff <classiczuma@gmail.com>
2023-03-02 23:14:55 -0700

    upload new python files

---
+++ draw.py
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+import turtle
+
+painter = turtle.Turtle()
+painter.pensize(20)
+
+for _ in range(18):
+    painter.forward(100)
+    painter.left(100)
+
+=======
+import turtle
+painter = turtle.Turtle()
+painter.forward(100)
+painter.left(90)
+>>>>>>> parent of 9de49b0 (repeat 4 times with a for loop)
+turtle.done()
